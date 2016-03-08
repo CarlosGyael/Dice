114255276bad809231134e96a0bc63d8e4aac2e9
jcanderson195 <jcanderson195@gmail.com>
2016-03-08 14:42:21 -0600

    updated menu with if user read rules

--- src/menu.java
+++ src/menu.java
@@ -1,8 +1,11 @@
-
+import java.util.Scanner;
 public class menu {
 
 	Time twoSecDelay = new Time();
-
+	
+	int choice = 0;
+	
+	Scanner keyboard =new Scanner (System.in); 
 	public void menu() {
 
 		System.out.println("Welcome to Dice Game!");
@@ -10,6 +13,20 @@ public class menu {
 		twoSecDelay.TwoSeconds();
 
 		System.out.println("Here are the rules: Player one goes first rolling the die. Then the computer or player 2 goes next rolling the die. Depending on the number in which you land on determines how far you move. For example, if you roll a 4, then you move forward 4 spaces. Also, depending on the size of the die that you choose will determine the maxium amount of spaces one can move. The size of the die available are: d4, d6, d8, d10, d12, d20. To win the game, the first player to 100 or more wins. ");
+	
+		System.out.println("Did you finish reading the rules?");
+		System.out.println("Press 1 for Yes");
+		
+		
+		choice = keyboard.nextInt();
+		
+		if (choice == 1){
+			
+		}
+		else {
+			System.out.println("Sorry we don't recognize that answer!");
+		}
+		
 	}
 
 }
