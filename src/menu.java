822397e216eb558b640f11c32af11dd5e261e383
jcanderson195 <jcanderson195@gmail.com>
2016-03-08 15:11:53 -0600

    Added while loops to (did u read the rules) and (single player,multiplayer, etc)

--- src/menu.java
+++ src/menu.java
@@ -5,6 +5,7 @@ public class menu {
 
 	static UserPlayer Intro = new UserPlayer();
 	
+	boolean run = true;
 	int choice = 0;
 	
 	Scanner keyboard =new Scanner (System.in); 
@@ -16,23 +17,27 @@ public class menu {
 
 		System.out.println("Here are the rules: Player one goes first rolling the die. Then the computer or player 2 goes next rolling the die. Depending on the number in which you land on determines how far you move. For example, if you roll a 4, then you move forward 4 spaces. Also, depending on the size of the die that you choose will determine the maxium amount of spaces one can move. The size of the die available are: d4, d6, d8, d10, d12, d20. To win the game, the first player to 100 or more wins. ");
 	
+		while(run){
 		System.out.println("Did you finish reading the rules?");
 		System.out.println("Press 1 for Yes");
 		
 		
 		choice = keyboard.nextInt();
-		
+	
 		if (choice == 1){
 			Intro.PlayerIntro();
 		}
 		else {
 			System.out.println("Sorry we don't recognize that answer!");
 		}
+		}
 		
 	}
 	
 	public void StartMenu(){
 		
+		
+		while (run){
 		System.out.println("Menu");
 		System.out.println("----");
 		
@@ -50,6 +55,8 @@ public class menu {
 			
 		}else {
 			System.out.println("Sorry we don't recognize that answer!");
+			
+		}
 		}
 	}
 	
