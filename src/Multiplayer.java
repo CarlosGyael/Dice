bccf472d63d5d27c5851741b3ee23b34e9941256
Carlos Gutierrez <carlos_gutierrezz99@hotmail.com>
2016-03-09 10:56:34 -0600

    added username 2 functions

--- src/Multiplayer.java
+++ src/Multiplayer.java
@@ -3,8 +3,26 @@ import java.util.Scanner;
 public class Multiplayer {
 
 	int choice = 0;
+	String Username2;
 	
 	Scanner keyboard =new Scanner(System.in);
+	
+	public String UserInput(){
+		
+		Username2 = keyboard.next();
+	
+		return Username2;
+	}
+	
+	public void PlayerTwo(){
+
+		System.out.println("Please enter a username:");
+		
+		UserInput();
+		
+		System.out.println("Hello " + Username2 + " Nice of you to play. ");
+	}
+		
 	
 	public void Multiplayer(){
 		
@@ -12,20 +30,6 @@ public class Multiplayer {
 
 		choice = keyboard.nextInt();
 		
-	/*public String UserInput(){
-			
-			Username = keyboard.next();
-		
-			return Username;
-		}
-		
-		public void PlayerTwo(){
-
-			System.out.println("Please enter a username:");
-			
-			UserInput();
-			
-			System.out.println("Hello " + Username + " Nice of you to play. ");*/
 		
 		if (choice == 1){
 			System.out.println("Awesome! Let's Start!");
