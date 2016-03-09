02afd9205424e6865946ac0aa30c7d41b494a4da
Carlos Gutierrez <carlos_gutierrezz99@hotmail.com>
2016-03-09 10:46:57 -0600

    Added User2 points and storage

--- src/PointEngine.java
+++ src/PointEngine.java
@@ -2,6 +2,8 @@
 public class PointEngine {
 	public int point = 0;
 	public int CompPoint = 0;
+	public int User2Point = 0;
+	
 	public int getPoints(){
 		return point;
 	}
@@ -11,9 +13,13 @@ public class PointEngine {
 	public int CompPoints(){
 		return CompPoint;
 	}
-	
 	public void addComp(int newpoints){
 		CompPoint+=newpoints;
 	}
-
+	public int User2Points(){
+		return User2Point;
+	}
+	public void addUser2(int newpoints){
+		User2Point+=newpoints;
+	}
 }
